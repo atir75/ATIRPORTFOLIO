9435d55ddcf5efd2cf5f6e7c7eb5efcf6ebf3463
Maria Rita <101608517+atir75@users.noreply.github.com>
2023-12-11 21:49:53 -0300

    wip: done project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/starbucks.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import CRUDC from "../../Assets/Projects/CRUDcshap.png";
+import starbucks from "../../Assets/Projects/starbucks.png";
+import Memorygame from "../../Assets/Projects/gameJSproj.png";
+import Java from "../../Assets/Projects/javaCRUD.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/projetoAPINode.png";
+import API from "../../Assets/Projects/projetoAPINode.png";
 
 function Projects() {
   return (
@@ -23,18 +23,17 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={Java}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Java CRUD"
+              description="A Java project, built with Maven, incorporates an external maps API for a people registration system. The application allows CRUD operations on individual records, emphasizing address management through map integration."
+              ghLink="https://github.com/atir75/Java-CRUD"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={API}
               isBlog={false}
               title="API Node.js with Docker"
               description="Embark on a Node.js API project that delves into Docker concepts, providing an opportunity to enhance proficiency in both Node.js and containerization principles."
@@ -44,39 +43,27 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={Memorygame}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
+              title="Memory game"
+              description="A memory game created with JavaScript to enhance programming concepts and skills in web development. The game focuses on improving programming logic, DOM manipulation, and user interactivity. It features card flipping, pair matching mechanics, and visual feedback to reinforce practical understanding of fundamental JavaScript concepts, providing a hands-on educational experience."
+              ghLink="https://github.com/atir75/Memory-Game"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={CRUDC}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="C# CRUD"
+              description="A C# CRUD application for book registration enables Create, Read, Update, and Delete operations. It offers a user-friendly interface for efficient management of book records, including features for adding, viewing, updating, and deleting entries, demonstrating practical implementation of CRUD functionality in C#."
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-            />
-          </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={starbucks}
               isBlog={false}
               title="A website for Starbucks"
               description="Crafted entirely with pure HTML and CSS, stands as one of the initial projects I undertook in frontend development. 
